3da6c85b9a4b96350f3e19ee9cea8d8e83f19b41
LamiaaQ <68843047+LamiaaQ@users.noreply.github.com>
2020-09-19 15:04:36 +0300

    order change in bugs routes

--- controller/bug.js
+++ controller/bug.js
@@ -1,170 +1,184 @@
-const mongoose = require('mongoose');
-const Bug = require('../model/bug');
+const mongoose = require("mongoose");
+const Bug = require("../model/bug");
 
-module.exports={
-    index: (req,res,next)=>{
-        Bug.find({}).then((bugs)=>{
-
-            res.locals.bugs = bugs;
-             next();
-
-        }).catch((error)=>{
-
-            console.log(error);
-        })
-    },
-
-    indexView:(req,res)=>{
-        res.render('bugs/index');
-    },
-
-    new: (req,res)=>{
-        res.render('bugs/new');
-    },
-    
-    create:(req,res,next)=>{
-        
-        new Bug({
-            title:req.body.title,
-            description:req.body.description,
-            image:req.body.image,
-            priority:req.body.priority,
-            status:'open',
-            issuedUserId:mongoose.Types.ObjectId(req.user._id),
-            programId:mongoose.Types.ObjectId(req.body.project)
-        }).save()
-        req.flash('success','تم حفظ المشكلة')
-        if(req.user.isDev){
-        res.locals.redirect = '/bugs';
-        }else {res.locals.redirect = '/bugs/new'}
+module.exports = {
+  index: (req, res, next) => {
+    Bug.find({})
+      .then((bugs) => {
+        res.locals.bugs = bugs;
         next();
-        
-    },
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
 
-    edit:(req,res)=>{
-        Bug.findById({_id:req.params.id}).then(bug=>{
-            res.locals.bug = bug;
-            res.render('bugs/edit');
-        })
-       
-    },
+  indexView: (req, res) => {
+    res.render("bugs/index");
+  },
 
-    update:(req,res,next)=>{
-        Bug.updateOne({_id: mongoose.Types.ObjectId(req.params.id)},{
-            title: req.body.title,
-            description: req.body.description,
-            priority:req.body.priority
-        }).then(
-            ()=>{
-                req.flash('success','تم تحديث تقرير المشكلة')
-              res.locals.redirect = '/bugs';
-              next();  
-            }
-        ).catch(error=>{
-            console.log('Error Occured In Updating a bug info:'+error);
-        })
-    },
+  new: (req, res) => {
+    res.render("bugs/new");
+  },
 
-    show:(req,res)=>{
-        Bug.findById({_id:req.params.id}).populate('issuedUserId').populate('programId').exec(function(err,bug){
-            if(err){
-                console.log(`error in show bug function: ${err}`);
-            }else{
-                res.locals.bug = bug;
-                res.render('bugs/show');
-            };
-        })
-    },
-    closeBug:(req,res,next)=>{
-
-        Bug.updateOne({_id: mongoose.Types.ObjectId(req.params.id)},{
-            status: 'closed',
-            devId: req.user._id,
-            closedDate: new Date()
-        }).then(
-            ()=>{
-                req.flash('success','تم إغلاق المشكلة بنجاح')
-                res.locals.redirect ='/bugs';
-              next();  
-            }
-        ).catch(error=>{
-            console.log(`Error Occured In assigned bug: ${error}`);
-        })
-    },
-
-    assignBug:(req,res,next)=>{
-
-        Bug.updateOne({_id: mongoose.Types.ObjectId(req.params.id)},{
-            status: 'assigned',
-            devId: req.user._id
-        }).then(
-            ()=>{
-                req.flash('success','تم حجز المشكلة بنجاح')
-                res.locals.redirect ='/bugs';
-              next();  
-            }
-        ).catch(error=>{
-            console.log('Error Occured In closing a bug:'+error);
-        })
-    },
-    showfilteredBugs: (req,res,next)=>{
-        Bug.find({status:req.params.status}).then((bugs)=>{
-
-            res.locals.bugs = bugs;
-             next();
-
-        }).catch((error)=>{
-
-            console.log(error);
-        })
-    },
-    assignedToMe: (req,res,next)=>{
-        Bug.find({devId:req.user._id}).then((bugs)=>{
-            res.locals.bugs = bugs;
-             next();
-
-        }).catch((error)=>{
-            console.log(error);
-        })
-    },
-    issuedBy: (req,res,next)=>{
-        Bug.find({issuedUserId: mongoose.Types.ObjectId(req.params.id)}).then((bugs)=>{
-            res.locals.bugs = bugs;
-             next();
-        }).catch((error)=>{
-            console.log(error);
-        })
-    },
-    searchForm:(req,res)=>{
-            res.render('bugs/search');
-    },
-    search:(req,res)=>{
-        Bug.find({title:req.body.title}).then((bugs)=>{
-            res.locals.bugs = bugs;
-            res.render('bugs/search');
-        }).catch((error)=>{
-            console.log(error);
-        })
-    },
-
-    delete: (req,res,next)=>{
-        //console.log('delete Method');
-        Bug.deleteOne({_id: req.params.id},function(err){
-            if (err){ 
-                console.log(err) 
-            } 
-            else{ 
-                console.log("Bug has been Deleted "); 
-                req.flash('error','تم حذف المشكلة')
-            } 
-            res.locals.redirect='/bugs';
-             next();
-        });     
-    },
-
-    redirectView:(req,res,next)=>{
-        let redirectPath = res.locals.redirect;
-        if(redirectPath){ res.redirect(redirectPath); 
-        }else{ next() }
+  create: (req, res, next) => {
+    new Bug({
+      title: req.body.title,
+      description: req.body.description,
+      image: req.body.image,
+      priority: req.body.priority,
+      status: "open",
+      issuedUserId: mongoose.Types.ObjectId(req.user._id),
+      programId: mongoose.Types.ObjectId(req.body.project),
+    }).save();
+    req.flash("success", "تم حفظ المشكلة");
+    if (req.user.isDev) {
+      res.locals.redirect = "/bugs";
+    } else {
+      res.locals.redirect = "/bugs/new";
     }
-}
+    next();
+  },
+
+  edit: (req, res) => {
+    Bug.findById({ _id: req.params.id }).then((bug) => {
+      res.locals.bug = bug;
+      res.render("bugs/edit");
+    });
+  },
+
+  update: (req, res, next) => {
+    Bug.updateOne(
+      { _id: mongoose.Types.ObjectId(req.params.id) },
+      {
+        title: req.body.title,
+        description: req.body.description,
+        priority: req.body.priority,
+      }
+    )
+      .then(() => {
+        req.flash("success", "تم تحديث تقرير المشكلة");
+        res.locals.redirect = "/bugs";
+        next();
+      })
+      .catch((error) => {
+        console.log("Error Occured In Updating a bug info:" + error);
+      });
+  },
+
+  show: (req, res) => {
+    Bug.findById({ _id: req.params.id })
+      .populate("issuedUserId")
+      .populate("programId")
+      .exec(function (err, bug) {
+        if (err) {
+          console.log(`error in show bug function: ${err}`);
+        } else {
+          res.locals.bug = bug;
+          res.render("bugs/show");
+        }
+      });
+  },
+  closeBug: (req, res, next) => {
+    Bug.updateOne(
+      { _id: mongoose.Types.ObjectId(req.params.id) },
+      {
+        status: "closed",
+        devId: req.user._id,
+        closedDate: new Date(),
+      }
+    )
+      .then(() => {
+        req.flash("success", "تم إغلاق المشكلة بنجاح");
+        res.locals.redirect = "/bugs";
+        next();
+      })
+      .catch((error) => {
+        console.log(`Error Occured In assigned bug: ${error}`);
+      });
+  },
+
+  assignBug: (req, res, next) => {
+    Bug.updateOne(
+      { _id: mongoose.Types.ObjectId(req.params.id) },
+      {
+        status: "assigned",
+        devId: req.user._id,
+      }
+    )
+      .then(() => {
+        req.flash("success", "تم حجز المشكلة بنجاح");
+        res.locals.redirect = "/bugs";
+        next();
+      })
+      .catch((error) => {
+        console.log("Error Occured In closing a bug:" + error);
+      });
+  },
+  showfilteredBugs: (req, res, next) => {
+    Bug.find({ status: req.params.status })
+      .then((bugs) => {
+        res.locals.bugs = bugs;
+        next();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+  assignedToMe: (req, res, next) => {
+    Bug.find({ devId: req.user._id, status: "assigned" })
+      .then((bugs) => {
+        res.locals.bugs = bugs;
+        next();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+  issuedBy: (req, res, next) => {
+    Bug.find({ issuedUserId: mongoose.Types.ObjectId(req.params.id) })
+      .then((bugs) => {
+        res.locals.bugs = bugs;
+        next();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+  searchForm: (req, res) => {
+    res.render("bugs/search");
+  },
+  search: (req, res) => {
+    Bug.find({ title: req.body.title })
+      .then((bugs) => {
+        res.locals.bugs = bugs;
+        res.render("bugs/search");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+
+  delete: (req, res, next) => {
+    //console.log('delete Method');
+    Bug.deleteOne({ _id: req.params.id }, function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Bug has been Deleted ");
+        req.flash("error", "تم حذف المشكلة");
+      }
+      res.locals.redirect = "/bugs";
+      next();
+    });
+  },
+
+  redirectView: (req, res, next) => {
+    let redirectPath = res.locals.redirect;
+    if (redirectPath) {
+      res.redirect(redirectPath);
+    } else {
+      next();
+    }
+  },
+};
--- routes/bugs.js
+++ routes/bugs.js
@@ -1,42 +1,73 @@
-const router = require('express').Router();
-const { body } = require('express-validator');
-const BugController = require('../controller/bug');
-const UserController = require('../controller/user');
-const ProgramController = require('../controller/program');
+const router = require("express").Router();
+const { body } = require("express-validator");
+const BugController = require("../controller/bug");
+const UserController = require("../controller/user");
+const ProgramController = require("../controller/program");
 
 /* show all bugs */
-router.get('/',UserController.restrictDev,BugController.index,BugController.indexView);
+router.get(
+  "/",
+  UserController.restrictDev,
+  BugController.index,
+  BugController.indexView
+);
 
 /* insert bug */
-router.get('/new',UserController.isAuth,ProgramController.index,BugController.new);
-router.post('/create',UserController.isAuth,[body('title').notEmpty(),body('description').notEmpty()],UserController.validator, BugController.create,BugController.redirectView);
+router.get(
+  "/new",
+  UserController.isAuth,
+  ProgramController.index,
+  BugController.new
+);
+router.post(
+  "/create",
+  UserController.isAuth,
+  [body("title").notEmpty(), body("description").notEmpty()],
+  UserController.validator,
+  BugController.create,
+  BugController.redirectView
+);
 
 /* updating bug info */
-router.get('/:id/edit',BugController.edit);
-router.put('/:id/update',BugController.update,BugController.redirectView);
+router.get("/:id/edit", BugController.edit);
+router.put("/:id/update", BugController.update, BugController.redirectView);
 
 /* show bug report*/
-router.get('/:id/show',BugController.show);
+router.get("/:id/show", BugController.show);
 
 /* closing a bug report*/
-router.get('/:id/close',BugController.closeBug,BugController.redirectView);
+router.get("/:id/close", BugController.closeBug, BugController.redirectView);
 /* assigning a bug */
-router.get('/:id/assign',BugController.assignBug,BugController.redirectView);
+router.get("/:id/assign", BugController.assignBug, BugController.redirectView);
 
 /* search for a bug */
-router.get('/search',BugController.search);
-router.post('/search',BugController.search);
-
-/* filter bugs*/ //why order matters??
-router.get('/:status',UserController.restrictDev,BugController.showfilteredBugs,BugController.indexView);
+router.get("/search", BugController.search);
+router.post("/search", BugController.search);
 
 /* search for bug assigned to the dev*/
-router.get('/assigned',UserController.restrictDev,BugController.assignedToMe,BugController.indexView);
+router.get(
+  "/assigned",
+  UserController.restrictDev,
+  BugController.assignedToMe,
+  BugController.indexView
+); //why order matters??
+
+/* filter bugs*/ router.get(
+  "/:status",
+  UserController.restrictDev,
+  BugController.showfilteredBugs,
+  BugController.indexView
+);
 
 /*  find bug issued by specific user*/
-router.get('/issuedBy/:id',UserController.restrictDev,BugController.issuedBy,BugController.indexView);
+router.get(
+  "/issuedBy/:id",
+  UserController.restrictDev,
+  BugController.issuedBy,
+  BugController.indexView
+);
 
 /* deleting bug */
-router.delete('/:id/delete',BugController.delete,BugController.redirectView);
+router.delete("/:id/delete", BugController.delete, BugController.redirectView);
 
-module.exports= router;
+module.exports = router;
